ccf26ee19c33d7e8ec42bf230c6fc6a4a81bedbb
liyao <915733588@qq.com>
2018-12-18 16:30:12 +0800

    vuetest

---
+++ precache-manifest.6187ea4a9a141d1492f5d00da03e30d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4338ed8ef3eb78adf4f",
+    "url": "/js/about.6a95e25f.js"
+  },
+  {
+    "revision": "a410fe809de0335cbef2",
+    "url": "/css/app.f02b2626.css"
+  },
+  {
+    "revision": "a410fe809de0335cbef2",
+    "url": "/js/app.40dfedce.js"
+  },
+  {
+    "revision": "2894918c2d3fad3f8337",
+    "url": "/js/chunk-vendors.8e475858.js"
+  },
+  {
+    "revision": "c1ba9f4b497ef1d9fba99cb792d5816f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
